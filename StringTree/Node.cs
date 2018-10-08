55e59ea940b79c14f06c8e93d496cd4a8e687eb9
adan.benitez <adan.benitez@loop.colum.edu>
2018-10-08 14:58:12 -0500

    Making Progress

--- StringTree/Node.cs
+++ StringTree/Node.cs
@@ -9,6 +9,48 @@ namespace StringTree
     class Node
     {
         //needs 3 other Child Nodes
-        //
+        public Node [] children;
+
+        //check when adding Child Nodes if those Nodes are all filled
+        private bool isFilled;
+
+        //Stores depth of node
+        public int depth;
+
+        //Stores String
+        public string nodeInfo;
+
+        //constructor
+        public Node()
+        {
+            //children = new Node[3];
+        }
+
+
+        //Traverse
+        
+
+        public void AddNode(Node newNode)
+        {
+            //checks if all children nodes are filled
+            if (!isFilled)
+            {
+                //walks through all childer until an empty one is found
+                for (int i = 0; i < children.Length; i++)
+                {
+                    if (children[i] == null)
+                    {
+                        children[i] = newNode;
+
+                        //if this is the last child to be filled change this Node's isFill to true
+                        if (i > 2)
+                        {
+                            isFilled = true;
+                        }
+                    }
+                }
+            }
+
+        }//end of AddNode
     }
 }
---
+++ StringTree/Tree.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringTree
+{
+    class Tree
+    {
+        //Root Node
+        Node root;
+
+        public bool IsReady { get; private set; }
+
+        Tree(string _fileName)
+        {
+            root = new Node();
+
+            IsReady = LoadText(_fileName);
+        }
+
+        public bool LoadText(string fileName)
+        {
+            //Convert Text File to a String Array 
+            string[] lines = File.ReadAllLines(fileName);
+
+            //going through each specific line
+            for (int i = 0; i < lines.Length; i++)
+            {
+                //stores string line
+                string temp = lines[i];
+                int count = 0; //keeps track of depth
+
+                for (int j = 0; j < temp.Length; j++)
+                {
+                    if (temp[i].Equals(' '))
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                
+            }
+            //check for IO errors and other exceptions
+            return false;
+
+        }
+    }
+}
